9f781f1a974fdac7e49b27aa5e2f4cf7867ffefd
keysummer <masakikato456@gmail.com>
2021-06-06 16:21:54 +0900

    setTimeoutでフレームレートを指定

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const medias = {
   audio: false,
-  video: true,
+  video: true
   // video: {
   //   facingMode: {
   //     exact: "environment"
@@ -8,32 +8,30 @@ const medias = {
   // }
 };
 const video = document.getElementById("video");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 const promise = navigator.mediaDevices.getUserMedia(medias);
 
 promise.then(successCallback)
-       .then(errorCallback);
-
-const wait = (sec) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(resolve, sec*1);
-    //setTimeout(() => {reject(new Error("エラー！"))}, sec*1000);
-  });
-};
+       .catch(errorCallback);
 
 function successCallback(stream) {
   video.srcObject = stream;
-  video.style.visibility="hidden";
   requestAnimationFrame(draw);
-};
+}
 
 function errorCallback(err) {
+  console.log(err);
   alert(err);
-};
+}
 
-async function draw() {
-  video.style.visibility="hidden";
-  await wait(8);
-  video.style.visibility="visible";
-  await wait(8);
-  requestAnimationFrame(draw);
+var fps = 10;
+
+function draw() {
+  setTimeout(function() {
+    requestAnimationFrame(draw);
+    canvas.width  = window.innerWidth;
+    canvas.height = window.innerHeight;
+    ctx.drawImage(video, 0, 0);
+  }, 1000/fps);
 }
