6782eb423c50a7f33a5dbdd121f6aecf428ff13f
keysummer <masakikato456@gmail.com>
2021-06-06 15:40:09 +0900

    カメラの表示非表示を繰り返す処理を追記

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const medias = {
   audio: false,
+  // video: true,
   video: {
     facingMode: {
       exact: "environment"
@@ -12,11 +13,27 @@ const promise = navigator.mediaDevices.getUserMedia(medias);
 promise.then(successCallback)
        .then(errorCallback);
 
+const wait = (sec) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, sec*1);
+    //setTimeout(() => {reject(new Error("エラー！"))}, sec*1000);
+  });
+};
+
 function successCallback(stream) {
   video.srcObject = stream;
-  // video.style.visibility="visible";
+  video.style.visibility="hidden";
+  requestAnimationFrame(draw);
 };
 
 function errorCallback(err) {
   alert(err);
 };
+
+async function draw() {
+  video.style.visibility="hidden";
+  await wait(5);
+  video.style.visibility="visible";
+  await wait(5);
+  requestAnimationFrame(draw);
+}
