531043957f712542a08f621edeb1c6840102e8c3
keysummer <masakikato456@gmail.com>
2021-06-06 15:00:32 +0900

    元の状態に戻す

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-// const _sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-const medias = {audio : false, video : {
-  facingMode : {
-    exact : "environment"
-  }
-}},
+const medias = {
+  audio : false,
+  video : true
+};
 const video = document.getElementById("video");
 const promise = navigator.mediaDevices.getUserMedia(medias);
 
@@ -12,25 +10,8 @@ promise.then(successCallback)
 
 function successCallback(stream) {
   video.srcObject = stream;
-  requestAnimationFrame(draw);
-  // while(true){
-  //   video.style.visibility="hidden";
-  //   await _sleep(10);
-  //   video.style.visibility="visible";
-  //   await _sleep(10);
-  // }
 };
 
 function errorCallback(err) {
   alert(err);
-};
-
-function draw() {
-  video.style.visibility="visible";
-  // video.style.visibility="hidden";
-  //   await _sleep(10);
-  //   video.style.visibility="visible";
-  //   await _sleep(10);
-
-  requestAnimationFrame(draw);
 };
